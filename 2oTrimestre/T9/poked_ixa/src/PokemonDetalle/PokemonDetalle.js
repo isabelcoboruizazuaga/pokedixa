85f474e93d706c8b6fdcd006043709a25a519be4
isabelcoboruizazuaga <59738137+isabelcoboruizazuaga@users.noreply.github.com>
2023-02-14 08:23:05 +0100

    pokemon Detalle mejorado

--- 2oTrimestre/T9/poked_ixa/src/PokemonDetalle/PokemonDetalle.js
+++ 2oTrimestre/T9/poked_ixa/src/PokemonDetalle/PokemonDetalle.js
@@ -3,25 +3,54 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 export default function PokemonCard(props) {
-    const {id}=useParams();    
+    const { id } = useParams();
     const [pokemon, setPokemon] = useState([]);
-    
+    const [tipos, setTipos] = useState([]);
+
     useEffect(() => cargaDatos(), []);
 
     function cargaDatos() {
-        fetch("https://pokeapi.co/api/v2/pokemon/"+id)
+        fetch("https://pokeapi.co/api/v2/pokemon/" + id)
             .then((response) => response.json())
             .then((datosApi) => {
-                console.log(datosApi)
                 setPokemon(datosApi);
-            });    
+                setTipos(tipos.concat(datosApi.types));
+            });
     }
-    
+
     return (
-        <div className="card" >
-            <h2>{pokemon.id}</h2>
-            <h1></h1>
-        </div>
+        <>
+            <h1>{pokemon.name}  N.º {pokemon.id}</h1>
+
+            <table>
+                <tbody>
+                    <tr>
+                        <td>Altura</td>
+                        <td>Peso</td>
+                    </tr>
+                    <tr>
+                        <td>{pokemon.height}m</td>
+                        <td>{pokemon.weight}kg</td>
+                    </tr>
+                    <tr>
+                        <td>Experiencia Base</td>
+                    </tr>
+                    <tr>
+                        <td>{pokemon.base_experience}</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <div>
+                <p>Tipos</p>
+                {
+                    tipos.map((tipo) =>
+                        <h3 key={tipo.type.name}>{tipo.type.name}</h3>
+                    )
+                }
+            </div>
+
+        </>
     );
 }
 
